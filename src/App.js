7aa5d7cb7d0c381948a56ad3381266d845721ee1
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-04 11:36:04 -0700

    completed fetching data for 20 random pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  // todo state for loading
+  const [isLoading, setIsLoading] = useState(true);
+  const [cards, setCards] = useState([]);
+  // todo state for cards
+
   // todo state for high score
   // todo state for current score
-  // todo state for cards
 
   // todo useEffect for pulling all the card info
   useEffect(() => {
-    const subscription = props.source.subscribe();
-    return () => {
-      subscription.unsubscribe();
-    };
+    const RANDOM_NUMBERS = [];
+    for (let i = 0; i < 20; i++) {
+      const RANDOM_NUMBER = Math.floor(Math.random() * 151 + 1);
+      RANDOM_NUMBERS.push(RANDOM_NUMBER);
+    }
+
+    RANDOM_NUMBERS.forEach((number) => {
+      // todo fetch data
+      (async function apiData() {
+        try {
+          const RESPONSE = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${number}`
+          );
+          const JSON = await RESPONSE.json();
+          const NAME = JSON.species.name;
+          const PICTURE = JSON.sprites.front_shiny;
+          setCards((prevState) => {
+            return [...prevState, { NAME, PICTURE }];
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    });
   }, []);
 
   return (
     <>
       <div></div>
+      {console.log(cards)}
     </>
   );
 }
