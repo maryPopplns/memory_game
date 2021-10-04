3e0f336cadaae5fd2654be787d76e1822bbd0954
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-04 10:35:15 -0700

    added boilerplate for fetching api data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // todo state for loading
+  // todo state for high score
+  // todo state for current score
+  // todo state for cards
+
+  // todo useEffect for pulling all the card info
+  useEffect(() => {
+    const subscription = props.source.subscribe();
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
   return (
     <>
       <div></div>
